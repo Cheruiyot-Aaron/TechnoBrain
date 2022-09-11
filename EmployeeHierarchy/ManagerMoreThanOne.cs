3b0a54af3bb45bcadcf6a7747a7aa8f27356bfcf
aaron cheruiyot <ACHERUIYOT090@GMAIL.COM>
2022-09-11 13:35:50 +0300

    Initial commit

---
+++ EmployeeHierarchy/ManagerMoreThanOne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EmployeeHierarchy
+{
+    [Serializable]
+    internal class ManagerMoreThanOne : Exception
+    {
+        public ManagerMoreThanOne()
+        {
+        }
+
+        public ManagerMoreThanOne(string message) : base(message)
+        {
+        }
+
+        public ManagerMoreThanOne(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ManagerMoreThanOne(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
